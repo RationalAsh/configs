cf2da16b0ec950f9c4ab969611818d61409a786a
RationalAsh <ashwinnarayan1994@gmail.com>
2014-07-14 11:47:47 +0530

    Update i3suspend.sh

--- i3suspend.sh
+++ i3suspend.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+#This is a script that suspends the computer
+#after one hour of inactivity. Uses the application
+#xprintidle. Install the app using 
+#"sudo apt-get install xprintidle" before running this
+#script
 
+#One hour in milliseconds
 idletime=$((1000*60*60))
-bufftime=$((1000*30))
 
 while true; do
     idle=`xprintidle`
     #echo "idle for $idle ms"
     if(($idle > $idletime)); then
 	echo "Time to sleep!"
+	#Shell command to suspend system
 	dbus-send --system --print-reply --dest=org.freedesktop.UPower /org/freedesktop/UPower "org.freedesktop.UPower.Suspend"
     fi
+    #Only check for inactivity once a minute.
     sleep 60
 done
